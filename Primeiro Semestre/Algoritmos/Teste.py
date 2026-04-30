fc296375a9c89b8848520f82bdf0177c23772e31
Joaquim Gomes <joaquimng07@gmail.com>
2026-04-30 11:02:14 -0300

    Add files via upload

---
+++ Primeiro Semestre/Algoritmos/Teste.py
@@ -0,0 +1,11 @@
+# idade para mudar os IFs
+Idade = 18
+
+if Idade >= 18:
+    print ("adulto")
+elif Idade >= 16:
+    print ("adolescente") 
+elif Idade >= 12:
+    print ("pré-adolescente") 
+else:
+    print ("criança") 
