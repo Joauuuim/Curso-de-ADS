6ff114f7cd4dff1574c9a650ea0c99142f275156
Joaquim Gomes <joaquimng07@gmail.com>
2026-04-30 11:04:18 -0300

    Delete Primeiro Semestre/Algoritmos/Teste2.py

--- Primeiro Semestre/Algoritmos/Teste2.py
+++
@@ -1,9 +0,0 @@
-Somando = 0
-# print("Digite os números")
-for i in range (1,10):
-    Numero = float (input (f"Digite o {i}o valor: "))
-    Somando += Numero
-
-print ("-" * 20)
-print (f"A soma total é de: {Somando}")
-print ("-" * 20)
